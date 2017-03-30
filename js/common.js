53ccdcfe8289321f313b403fb245ef9852aedca5
nukenukem4 <nukenukem@yandex.ru>
2017-03-30 18:31:52 +0300

    final

--- js/common.js
+++ js/common.js
@@ -1,28 +1,12 @@
 
 var headings = document.getElementsByClassName("head");
-
 function showPanel(e) {
 	var heading = e.target;
-	var panel = heading.nextElementSibling;
-	panel.classList.toggle("show");
-
+	var panel = heading.parentNode.parentNode.nextElementSibling;
+    var arrow = heading.parentNode.parentNode.getElementsByClassName("icon")[0];
+    arrow.classList.toggle("rotate");
+    panel.classList.toggle("show");
 };
-
 for(var i = 0; i < headings.length; i++) {
 	headings[i].onclick = showPanel;
 };
-
-//var ali = document.getElementsByClassName("icon")
-//
-//function arrow(event) {
-//	console.log(event.target);
-//	event.target.classList.toggle("qwe");
-//
-//};
-//
-//
-//
-//for( var i = 0; i < ali.length; i++) {
-//	ali[i].onclick = arrow;
-//}
-
